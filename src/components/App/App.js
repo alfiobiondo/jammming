eeffe27414db6de68a1f905b00d47ae160f1934e
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-10-26 15:09:43 +0200

    add css and title to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 const App = () => {
-	const [playlist, setPlaylist] = useState('');
+	const [searchResults, setSearchResult] = useState([]);
+	const [playlistName, setPlaylistName] = useState('New Playlist');
+	const [playlistTracks, setPlaylistTracks] = useState([]);
 
-	return <div></div>;
+	return (
+		<>
+			<h1>
+				Ja<span className='highlight'>mmm</span>ing
+			</h1>
+			<div className='App'>
+				<SearchBar />
+				<div>
+					<SearchResults />
+					<Playlist />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default App;
