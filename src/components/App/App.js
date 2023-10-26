243a4b6b7dc795ea3e37341d9719db062a581a95
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-10-26 12:18:51 +0200

    organize folders

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+	const [playlist, setPlaylist] = useState('');
+
+	return <div></div>;
+};
+
+export default App;
