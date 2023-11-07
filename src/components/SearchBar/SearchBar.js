4f514e5f175b77ad7ced9eda4d9df763d4c2f5ae
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2023-11-07 11:56:50 +0100

    implement components and api Spotify

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-function SearchBar() {}
+import './SearchBar.css';
+
+function SearchBar(props) {
+	const [term, setTerm] = useState('');
+
+	const handleTermChange = useCallback((event) => {
+		setTerm(event.target.value);
+	}, []);
+
+	const search = useCallback(() => {
+		props.onSearch(term);
+	}, [props.onSearch, term]);
+
+	return (
+		<div className='SearchBar'>
+			<input
+				placeholder='Enter A Song Title'
+				onChange={handleTermChange}></input>
+			<button className='SearchButton' onClick={search}>
+				SEARCH
+			</button>
+		</div>
+	);
+}
 
 export default SearchBar;
